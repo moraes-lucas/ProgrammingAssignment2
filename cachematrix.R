9b4b6dfe1b3f82cdca8d071e089156eec7fd0fc6
Lucas Barata de Moraes <lbdemoraes@gmail.com>
2019-07-26 17:19:16 -0300

    Edited functions for the ProgrammingAssignment2 by Lucas Moraes.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## The first function, makeCacheMatrix creates a special "matrix", 
-## which is really a list containing a function to
+## which is really a list containing a function to:
 
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
